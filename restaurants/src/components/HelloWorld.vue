665373f16e4e4d30f7f2769cdd1cee6209793d15
camaramamadou92 <71674569+camaramamadou92@users.noreply.github.com>
2021-10-18 21:37:30 +0200

    forme du document

--- restaurants/src/components/HelloWorld.vue
+++ restaurants/src/components/HelloWorld.vue
@@ -2,66 +2,81 @@
   <div class="hello">
     <!--<button v-on:click="getRestaurantsFromServer()">test recuperation des restaurants</button>-->
 
-<md-table>
-        <md-table-row >
-            <md-table-cell > <img src="../restaurants/image/miage.jpg" /></md-table-cell>
-            
-            <!--<md-table-cell><img src="../restaurants/image/logo2.png" height="500%" width="500%"/></md-table-cell>-->
-        </md-table-row>
+    <md-table>
+      <md-table-row>
+        <md-table-cell>
+          <img src="../restaurants/image/miage.jpg"
+        /></md-table-cell>
+
+        <!--<md-table-cell><img src="../restaurants/image/logo2.png" height="500%" width="500%"/></md-table-cell>-->
+      </md-table-row>
     </md-table>
     <md-table>
-        <md-table-row>
-            <md-table-cell > 
-                <h1> PROJET TECHNOLOGIE WEB 2021 - 2022 </h1>
-                </md-table-cell>
-        </md-table-row>
+      <md-table-row>
+        <md-table-cell>
+          <h1>PROJET TECHNOLOGIE WEB 2021 - 2022</h1>
+        </md-table-cell>
+      </md-table-row>
     </md-table>
- <!--ajout menu-->
+    <!--ajout menu-->
     <md-menu md-direction="bottom-start">
       <md-button md-menu-trigger>MENU</md-button>
 
       <md-menu-content>
-          
         <md-menu-item>A PROPOS</md-menu-item>
         <md-menu-item>CONTACTS</md-menu-item>
       </md-menu-content>
     </md-menu>
     <!---->
-    <form @submit.prevent ="ajouterRestaurant(event)">
-    
-        <label>
-            Nom : <input name = "name" type="text" required v-model="nom">
-        </label>
-        <label>
-            Cuisine : <input name = "cuisine" type="text" required v-model="cuisine">
-        </label>
+    <form @submit.prevent="ajouterRestaurant(event)">
+      <label>
+        Nom : <input name="name" type="text" required v-model="nom" />
+      </label>
+      <label>
+        Cuisine :
+        <input name="cuisine" type="text" required v-model="cuisine" />
+      </label>
 
-        <md-button class="md-raised">Ajouter</md-button>
+      <md-button class="md-raised">Ajouter</md-button>
     </form>
 
-    <h1>Nombre de restaurants : {{nbRestaurantsTotal}}</h1>
+    <h1>Nombre de restaurants : {{ nbRestaurantsTotal }}</h1>
     <h2>
-        <label>Recherche par nom: 
-             <md-field md-clearable class="md-toolbar-section-end">
-            <md-input type="text" v-model="nomRecherche" placeholder="Seuda" @input="getRestaurantsFromServer()"/>
-         </md-field>
-        </label>
-
-        
+      <label
+        >Recherche par nom:
+        <md-field md-clearable class="md-toolbar-section-end">
+          <md-input
+            type="text"
+            v-model="nomRecherche"
+            placeholder="Seuda"
+            @input="getRestaurantsFromServer()"
+          />
+        </md-field>
+      </label>
     </h2>
-    <p> Nombre de pages total : {{nbPagesTotal}}</p>
-    <p>Nb de restaurants par page : 
-        <input type="range" min=5 max=100 step=1 
-            v-model="pagesize"
-            v-on:input="getRestaurantsFromServer()"
-        > {{pagesize}}
+    <p>Nombre de pages total : {{ nbPagesTotal }}</p>
+    <p>
+      Nb de restaurants par page :
+      <input
+        type="range"
+        min="5"
+        max="100"
+        step="1"
+        v-model="pagesize"
+        v-on:input="getRestaurantsFromServer()"
+      />
+      {{ pagesize }}
     </p>
 
-    <md-button :disabled = "page===0" @click="pagePrecedente">Précédent</md-button>
-    <md-button :disabled = "page===nbPagesTotal" @click="pageSuivante">Suivant</md-button>
-    <br>
+    <md-button :disabled="page === 0" @click="pagePrecedente"
+      >Précédent</md-button
+    >
+    <md-button :disabled="page === nbPagesTotal" @click="pageSuivante"
+      >Suivant</md-button
+    >
+    <br />
     <md-table>
-       <!-- <md-table-row >
+      <!-- <md-table-row >
             <md-table-head >Nom</md-table-head>
             <md-table-head>Cuisine </md-table-head>
             <md-table-head>Ville </md-table-head>
@@ -74,119 +89,128 @@
                 <md-table-cell> {{r.borough}}</md-table-cell>
             </md-table-row> -->
 
-            <md-table v-model="restaurants" md-sort="name" md-sort-order="asc" md-card md-fixed-header>
-            <md-table-toolbar>
-              <h1 class="md-title">Liste restaurants</h1>
-            </md-table-toolbar>
-      
-            <md-table-row slot="md-table-row" slot-scope="{ item }">
-              <md-table-cell md-label="Name" md-sort-by="name">{{ item.name }}</md-table-cell>
-              <md-table-cell md-label="Cuisine" md-sort-by="cuisine">{{ item.cuisine }}</md-table-cell>
-              <md-table-cell md-label="Ville">{{ item.borough }}</md-table-cell>
-            </md-table-row>
+      <md-table
+        v-model="restaurants"
+        md-sort="name"
+        md-sort-order="asc"
+        md-card
+        md-fixed-header
+      >
+        <md-table-toolbar>
+          <h1 class="md-title">Liste restaurants</h1>
+        </md-table-toolbar>
 
-            <md-table-empty-state md-label="aucun restaurant trouvé" 
-        :md-description="`aucun restaurant ne correspond à votre recherche pour '${nomRecherche}'. Essayez un autre nom de recherche ou ajoutez un nouveau nom restaurant.`">
+        <md-table-row slot="md-table-row" slot-scope="{ item }">
+          <md-table-cell md-label="Name" md-sort-by="name">{{
+            item.name
+          }}</md-table-cell>
+          <md-table-cell md-label="Cuisine" md-sort-by="cuisine">{{
+            item.cuisine
+          }}</md-table-cell>
+          <md-table-cell md-label="Ville">{{ item.borough }}</md-table-cell>
+        </md-table-row>
 
+        <md-table-empty-state
+          md-label="aucun restaurant trouvé"
+          :md-description="`aucun restaurant ne correspond à votre recherche pour '${nomRecherche}'. Essayez un autre nom de recherche ou ajoutez un nouveau nom restaurant.`"
+        >
         </md-table-empty-state>
-          </md-table>
-       
+      </md-table>
     </md-table>
-    
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   data: () => ({
-            restaurants: [],
-            nom: '',
-            cuisine: '',
-            nbRestaurantsTotal: 0,
-            page: 0,
-            pagesize: 10,
-            nbPagesTotal: 0,
-            nomRecherche: "",
-            lien: "http://localhost:8080/api/restaurants"
-        }),
-        mounted() {
-            console.log("avant rendu HTML")
-            this.getRestaurantsFromServer();
-        },
-        methods: {
-            pagePrecedente() {
-                if (this.page === 0) return
-                this.page--;
-                this.getRestaurantsFromServer();
-            },
-            pageSuivante() {
-                if (this.page === this.dernierePage) return
-                this.page++;
-                this.getRestaurantsFromServer();
-            },
-            getRestaurantsFromServer() {
-                let url = this.lien;
-                url += "?page=" + this.page;
-                url += "&pagesize=" + this.pagesize;
-                url += "&name=" + this.nomRecherche;
+    restaurants: [],
+    nom: "",
+    cuisine: "",
+    nbRestaurantsTotal: 0,
+    page: 0,
+    pagesize: 10,
+    nbPagesTotal: 0,
+    nomRecherche: "",
+    lien: "http://localhost:8080/api/restaurants",
+  }),
+  mounted() {
+    console.log("avant rendu HTML");
+    this.getRestaurantsFromServer();
+  },
+  methods: {
+    pagePrecedente() {
+      if (this.page === 0) return;
+      this.page--;
+      this.getRestaurantsFromServer();
+    },
+    pageSuivante() {
+      if (this.page === this.dernierePage) return;
+      this.page++;
+      this.getRestaurantsFromServer();
+    },
+    getRestaurantsFromServer() {
+      let url = this.lien;
+      url += "?page=" + this.page;
+      url += "&pagesize=" + this.pagesize;
+      url += "&name=" + this.nomRecherche;
 
-                fetch(url)
-                    .then((responseJSON) => {
-                        responseJSON.json().then((resJS) => {
-                            //afficheResponseGET(res);
-                            //console.log(resJS)
-                            this.restaurants = resJS.data;
-                            this.nbRestaurantsTotal = resJS.count;
-                            this.nbPagesTotal = Math.round(this.nbRestaurantsTotal / this.pagesize);
-                        });
-                    })
-                    .catch(function (err) {
-                        console.log(err);
-                    });
-            },
-            async supprimerRestaurant(id) {
-                //this.restaurants.splice(index, 1);
-                try {
-                    await fetch(this.lien + "/" + id, {
-                        method:"DELETE"
-                    });
-                    this.getRestaurantsFromServer();
-                } catch (error) {
-                    console.log("erreur pour delete " + error.msg)
-                }
-            },
-            async ajouterRestaurant() {
-                // eviter le comportement par defaut
-                //event.preventDefault();
+      fetch(url)
+        .then((responseJSON) => {
+          responseJSON.json().then((resJS) => {
+            //afficheResponseGET(res);
+            //console.log(resJS)
+            this.restaurants = resJS.data;
+            this.nbRestaurantsTotal = resJS.count;
+            this.nbPagesTotal = Math.round(
+              this.nbRestaurantsTotal / this.pagesize
+            );
+          });
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+    },
+    async supprimerRestaurant(id) {
+      //this.restaurants.splice(index, 1);
+      try {
+        await fetch(this.lien + "/" + id, {
+          method: "DELETE",
+        });
+        this.getRestaurantsFromServer();
+      } catch (error) {
+        console.log("erreur pour delete " + error.msg);
+      }
+    },
+    async ajouterRestaurant() {
+      // eviter le comportement par defaut
+      //event.preventDefault();
 
-                /* this.restaurants.push(
+      /* this.restaurants.push(
                      {
                          name: this.nom,
                          cuisine: this.cuisine
                      }
                  );*/
 
-                let donneesFormulaire = new FormData();
-                donneesFormulaire.append("nom", this.nom);
-                donneesFormulaire.append("cuisine", this.cuisine);
+      let donneesFormulaire = new FormData();
+      donneesFormulaire.append("nom", this.nom);
+      donneesFormulaire.append("cuisine", this.cuisine);
 
-               await fetch(this.lien, {
-                    method: "POST",
-                    body:donneesFormulaire
-                })
-                this.nom = "";
-                this.cuisine = "";
+      await fetch(this.lien, {
+        method: "POST",
+        body: donneesFormulaire,
+      });
+      this.nom = "";
+      this.cuisine = "";
 
-                this.getRestaurantsFromServer();
-            },
-            getColor(index) {
-                return (index % 2) ? 'lightBlue' : 'pink';
-            }
-
-        }
-    
-}
+      this.getRestaurantsFromServer();
+    },
+    getColor(index) {
+      return index % 2 ? "lightBlue" : "pink";
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
