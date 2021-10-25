b03aa20f6c265c7decc5752a4ea6ec4cce751c8f
camaramamadou92 <71674569+camaramamadou92@users.noreply.github.com>
2021-10-25 22:33:16 +0200

    detail du restaurant

--- restaurants/src/App.vue
+++ restaurants/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <router-link to="/">[Home]</router-link>
-    <router-link to="/restaurant">[Detail d'un Restaurants]</router-link>
     <router-view> </router-view>
     <HelloWorld />
     
--- restaurants/src/components/Restaurant.vue
+++ restaurants/src/components/Restaurant.vue
@@ -2,13 +2,13 @@
     <div>
       <!--<h1>Détail d'un restaurant </h1>-->
         <h1>Détail du restaurant qui a pour id : {{id}} </h1>
-        <!--<p>Cuisine : {{restaurant.cuisine}}</p>-->
-        <!--
+        
+        
         <ul>
             <li>Nom : {{restaurant.name}}</li>
             <li>Cuisine : {{restaurant.cuisine}}</li>
             
-        </ul>-->
+        </ul>
             
     </div>
 </template>
@@ -33,7 +33,7 @@ export default {
   mounted(){
     console.log("Avant affichage, on pourra faire un fetch...");
     console.log("ID = " + this.id)
-    let url = "http://localhost:8080/restaurants" + this.id;
+    let url = "http://localhost:8080/api/restaurants/" + this.id;
     fetch(url)
     .then(reponse =>{
       return reponse.json();
